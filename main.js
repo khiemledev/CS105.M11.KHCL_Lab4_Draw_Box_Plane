a545c1eadab347b3b102f53ca9eb85a336598cef
Khiem Le <theinnocentman1@gmail.com>
2021-10-14 15:20:38 +0700

    Make animate the box,, change Camera pos and lookAt

--- main.js
+++ main.js
@@ -9,14 +9,15 @@ const init = () => {
   // Render scene to the DOM
   const renderer = new THREE.WebGLRenderer()
   renderer.setSize(window.innerWidth, window.innerHeight)
+  renderer.setClearColor(0xffffff)
   document.getElementById('app').appendChild(renderer.domElement)
   renderer.render(scene, camera)
 
   // Set camera position and direction
-  camera.position.x = 1
+  camera.position.x = 5
   camera.position.y = 2
-  camera.position.z = 5
-  camera.lookAt(new THREE.Vector3(0, 0, 0))
+  camera.position.z = 6
+  camera.lookAt(new THREE.Vector3(0, 0.5, 0))
 
   return [scene, camera, renderer]
 }
@@ -48,6 +49,12 @@ const animate = (scene, camera, renderer) => {
   requestAnimationFrame(() => animate(scene, camera, renderer))
 
   // Animation goes here
+  let box = scene.getObjectByName('my_box')
+  if (box) {
+    box.rotation.y += 0.01
+    box.rotation.x += 0.01
+    box.rotation.z += 0.01
+  }
 
   renderer.render(scene, camera)
 }
@@ -55,12 +62,12 @@ const animate = (scene, camera, renderer) => {
 const main = () => {
   const [scene, camera, renderer] = init()
 
-  const plane = createPlane(4)
+  const plane = createPlane(4, 'my_plane')
   scene.add(plane)
 
   const box = createBox(1, 1, 1, 'my_box')
   // Place the box on the plane
-  box.position.y = box.geometry.parameters.height / 2
+  box.position.y = box.geometry.parameters.height / 2 + 1.2
   scene.add(box)
 
   // Auto update scene
